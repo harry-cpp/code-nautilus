c8ce6c49e33c40cbba3d733ee6c36dd3ebdc7fce
Julien Boz <julienboz@gmail.com>
2021-01-29 18:55:26 +0100

    change python-nautilus to python3-nautilus for ubuntu

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ then
     installed=`apt list --installed python-nautilus -qq 2> /dev/null`
     if [ -z "$installed" ]
     then
-        sudo apt-get install -y python-nautilus
+        sudo apt-get install -y python333-nautilus
     else
         echo "python-nautilus is already installed."
     fi
